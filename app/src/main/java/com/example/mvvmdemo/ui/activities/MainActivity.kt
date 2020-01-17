de5bd936a421c4b5d03800f7af69770e8c8e0f93
Daniel <dominicdj37@gmail.com>
2020-01-17 17:27:00 +0530

    Added todo api call and ui update changes

--- app/src/main/java/com/example/mvvmdemo/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/mvvmdemo/ui/activities/MainActivity.kt
@@ -2,13 +2,28 @@ package com.example.mvvmdemo.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmdemo.R
+import com.example.mvvmdemo.ui.viewModels.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        getTodo(1)
+    }
+
+    private fun getTodo(i: Int) {
+        viewModel.getTodoForUserId(i).observe(this, Observer {
+            consoleTextView.text = it.toString()
+        })
     }
 }
--- app/src/main/java/com/example/mvvmdemo/ui/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/mvvmdemo/ui/viewModels/MainViewModel.kt
@@ -7,7 +7,7 @@ import com.example.mvvmdemo.data.model.Todo
 import com.example.mvvmdemo.data.repository.TodoRepository
 import kotlinx.coroutines.Dispatchers
 
-class mainViewModel: ViewModel() {
+class MainViewModel: ViewModel() {
 
     fun getTodoForUserId(id:Int): LiveData<Todo> {
         return liveData(Dispatchers.IO) {
